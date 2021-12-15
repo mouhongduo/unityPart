570bee1bfa8770156b883cfe66b5162cbbdfeacc
mouhongduo <51589624+mouhongduo@users.noreply.github.com>
2021-12-15 16:51:14 +0800

    增加了psd2ugui的工具和prefab导出为lua文件的插件(待修改)

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 ## -以unity为基础的游戏后端 + 以unity为基础的RPG游戏客户端
 客户端URL:git@github.com:mouhongduo/unityPart.git  
 服务器URL:git@github.com:mouhongduo/skynetPart.git
-### 以unity为基础的游戏后端
+### 以unity为基础的游戏客户端
+#### 一、我的引用
+1.https://github.com/Tencent/xlua/ 蹭蹭热度
+2.https://github.com/zs9024/quick_psd2ugui PSD2UGUI的photoshop插件
+2.https://github.com/zs9024/quick_uicode 与上述插件配套的快速生成UI代码的生成器
---
+++ unityPart/Assets/Scripts/Lua/UI/View/TestView.lua
@@ -0,0 +1,41 @@
+--this file is auto created by QuickCode,you can edit it 
+--do not need to care initialization of ui widget any more 
+--------------------------------------------------------------------------------
+--/**
+--* @author :
+--* date    :
+--* purpose :
+--*/
+--------------------------------------------------------------------------------
+require "class"
+
+
+local TestView = class("TestView");
+
+function TestView:ctor(...)
+	--assign transform by your ui framework
+	self.transform = nil;
+end
+
+--region UI Variable Assignment 
+function TestView:InitUI()
+	self.canvasscaler_Test = self.transform:GetComponent("CanvasScaler"); 
+	self.graphicraycaster_Test = self.transform:GetComponent("GraphicRaycaster"); 
+	self.image_Image = self.transform:Find("Image"):GetComponent("Image"); 
+	self.inputfield_Image = self.transform:Find("Image"):GetComponent("InputField"); 
+	self.image_InputField = self.transform:Find("InputField"):GetComponent("Image"); 
+	self.inputfield_InputField = self.transform:Find("InputField"):GetComponent("InputField"); 
+	self.text_Placeholder = self.transform:Find("InputField/Placeholder"):GetComponent("Text"); 
+	self.text_Text = self.transform:Find("InputField/Text"):GetComponent("Text"); 
+	self.text_Text9 = self.transform:Find("Text"):GetComponent("Text"); 
+
+end
+--endregion 
+
+--region UI Event Register 
+function TestView:AddEvent()
+
+end
+--endregion 
+
+return TestView;
